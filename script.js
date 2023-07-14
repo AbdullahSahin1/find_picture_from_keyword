eef71b052c033451c6573c32055446c6bafdee02
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2023-07-14 21:54:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const formWrap = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrap = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#search");
+const clearButton = document.querySelector("#clear");
+const imagesWrap = document.querySelector(".images-wrapper")
+
+runEventListener();
+
+function runEventListener(){
+    form.addEventListener("submit",search);
+    clearButton.addEventListener("click", clear);
+}
+
+
+function clear(){
+    searchInput.value = "";
+    Array.from(imagesWrap.children).forEach((child)=>child.remove());
+    //imagesWrap.innerHTML = ""; (diğer yol)
+
+}
+
+function search(e){
+
+    const value = searchInput.value.trim();
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`,{
+        method : "GET" ,
+        headers : {
+            Authorization : "Client-ID 8VV7VbCw6yPfWaZClYwgLg_lYLIt5DbNPfS8MsiK528"
+        }
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+        Array.from(data.results).forEach((image)=>{
+            //console.log(image.urls.small);
+            addImageToUI(image.urls.small)
+        })
+    })
+    .catch((err)=> console.log(err))
+
+    e.preventDefault();
+}
+
+
+
+
+
+
+function addImageToUI(url){
+    const div = document.createElement("div");
+    div.className = "card";
+    const img = document.createElement("img");
+    img.setAttribute("src",url);
+    img.height = '300';
+    img.width = '300';
+    div.append(img);
+    imagesWrap.append(div);
+}
